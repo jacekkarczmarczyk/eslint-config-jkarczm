ae05bdcefc578d51b3677c0e1be1ddbdf4e1651e
Jacek Karczmarczyk <jkarczm@gmail.com>
2020-09-01 11:46:12 +0200

    fix: bring back vue/match-component-file-name for .vue files

--- vue.js
+++ vue.js
@@ -32,6 +32,10 @@ module.exports = {
     'vue/padding-line-between-blocks': ['error', 'always'],
     'vue/static-class-names-order': 'error',
     'vue/require-name-property': 'error',
+    'vue/match-component-file-name': ['error', {
+      extensions: ['vue'],
+      shouldMatchCase: true,
+    }],
     'vue/no-reserved-component-names': 'error',
 
     // @typescript-eslint/recommended overrides
